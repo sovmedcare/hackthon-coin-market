55ef82ef526de18a2310ddee9f6dff3a2a2cd56c
Quentin.Tu <tost84226@gmail.com>
2017-12-14 11:52:34 +0800

    temp

--- src/createChart.js
+++ src/createChart.js
@@ -1,27 +1,17 @@
 const g2 = require('@antv/g2')
 
-const data = [
-  { year: '1991', value: 3 },
-  { year: '1992', value: 4 },
-  { year: '1993', value: 3.5 },
-  { year: '1994', value: 5 },
-  { year: '1995', value: 4.9 },
-  { year: '1996', value: 6 },
-  { year: '1997', value: 7 },
-  { year: '1998', value: 9 },
-  { year: '1999', value: 13 }
-];
-module.exports = function create() {
+module.exports = function create(data) {
+  console.log(data)
   const _chart = new g2.Chart({
     container: 'chart',
     forceFit: true,
     height: window.innerHeight
   });
   _chart.source(data);
-  _chart.scale('value', {
+  _chart.scale(1, {
     min: 0
   });
-  _chart.scale('year', {
+  _chart.scale(0, {
     range: [ 0 , 1 ]
   });
   _chart.tooltip({
